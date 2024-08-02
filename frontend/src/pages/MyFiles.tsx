41607ff3ea0d88e6dbd2ebbe9563c76add8aed7a
Lui <6627294+yablochko8@users.noreply.github.com>
2024-08-02 10:39:21 -0400

    add MyFiles page

---
+++ frontend/src/pages/MyFiles.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * MyFiles page: shows you all your files
+ */
+const MyFiles: React.FC = () => {
+
+
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[80vw] bg-gray-100">
+      This is the MyFiles page
+    </div>
+  );
+};
+
+export default MyFiles;
